23c72ed82e443c769a83996913a0bdca4637ee62
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-09-17 23:26:50 +0200

    Fix styling error

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-//import Button from '@material-ui/core/Button';
-//import IconButton from '@material-ui/core/IconButton';
-//import MenuIcon from '@material-ui/icons/Menu';
+
+import CreateDialog from '../../containers/Exercises/Dialogs/Create';
 
 const Header = props => {
-    const classes = makeStyles(theme => ({
+    const useStyles = makeStyles(theme => ({
         root: {
             flexGrow: 1,
         },
@@ -21,12 +20,15 @@ const Header = props => {
         },
     }));
 
+    const classes = useStyles();
+
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" className={classes.title}>
                     Exercise Database
                 </Typography>
+                <CreateDialog />
             </Toolbar>
         </AppBar>
     );
