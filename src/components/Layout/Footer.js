6370d1154db9e78ce39a6239dd64974672d618cb
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-09-17 20:36:24 +0200

    Set footer to be clickable to switch between category

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -4,16 +4,28 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
 const Footer = props => {
+    const { muscles, onSelect, category } = props;
+
+    const index = category
+        //Index + 1 because of the default Tab with 'All' label taking the first index
+        ? muscles.findIndex(group => group === category) + 1
+        : 0;
+
+    const onIndexSelect = (e, index) => {
+        onSelect(index === 0 ? '' : muscles[index - 1]);
+    };
+
     return (
         <Paper>
             <Tabs
-                value={0}
+                value={index}
+                onChange={onIndexSelect}
                 indicatorColor="primary"
                 textColor="primary"
                 centered>
                 <Tab label='All' />
-                {props.muscles.map(muscle => (
-                    <Tab key={muscle.id} label={muscle.value} />
+                {muscles.map((muscle, index) => (
+                    <Tab key={index} label={muscle} />
                 ))}
             </Tabs>
         </Paper>
