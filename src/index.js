5e9b31179ce5af2da880c036e5f0e9249c3b145e
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-09-18 11:48:54 +0200

    Add Redux debugger

--- src/index.js
+++ src/index.js
@@ -14,7 +14,10 @@ const rootReducer = combineReducers({
     exercises: exerciseReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
     <Provider store={store}>
