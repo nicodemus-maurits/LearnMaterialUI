1e3590bc690d8a53d85e83abc5fe2e4b3d3ece1c
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-09-19 21:19:00 +0200

    Render item conditionally on normal or update exercise state

--- src/containers/Exercises/RightPane.js
+++ src/containers/Exercises/RightPane.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
 import { Paper, Typography } from '@material-ui/core';
+import ExerciseForm from '../../components/Forms';
 
 const RightPane = props => {
-    const { selectedExercise, style } = props;
-    const {
-        title = 'Welcome',
-        description = 'Please select Exercise from the left pane'
-    } = selectedExercise;
+    const { style } = props;
+    const { title, description } = props.selectedExercise;
+
+    let content = <Fragment>
+        <Typography variant='subtitle1'>
+            {title}
+        </Typography>
+        <Typography
+            variant='subtitle2'
+            style={{ marginTop: 20 }}>
+            {description}
+        </Typography>
+    </Fragment>;
+
+    if (props.editMode) {
+        content = <ExerciseForm formAction='update' />
+    }
 
     return (
         <Paper style={style}>
-            <Typography variant='subtitle1'>
-                {title}
-            </Typography>
-            <Typography
-                variant='subtitle2'
-                style={{ marginTop: 20 }}>
-                {description}
-            </Typography>
+            {content}
         </Paper>
     );
 };
 
-export default RightPane;
+const mapStateToProps = state => {
+    return {
+        selectedExercise: state.selExercise.selectedExercise,
+        editMode: state.selExercise.editMode
+    }
+}
+
+export default connect(mapStateToProps)(RightPane);
